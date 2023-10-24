195b4662042914e63f5ae70b74f78fbebc0e7be2
Adam Berkeš <berkeada@fit.cvut.cz>
2023-10-24 14:04:54 +0200

    Initialize a new Spring Boot project using spring initializr

---
+++ src/main/java/cz/cvut/fit/tjv/habitforgeserver/HabitforgeServerApplication.java
@@ -0,0 +1,13 @@
+package cz.cvut.fit.tjv.habitforgeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitforgeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitforgeServerApplication.class, args);
+	}
+
+}
