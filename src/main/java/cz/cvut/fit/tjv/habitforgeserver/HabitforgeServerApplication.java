f0a3ef2e9987a4cd6989a4a9d369334ee3c54fbe
Adam Berkeš <berkeada@fit.cvut.cz>
2024-01-02 18:41:16 +0100

    Format HabitforgeServerApplication

--- src/main/java/cz/cvut/fit/tjv/habitforgeserver/HabitforgeServerApplication.java
+++ src/main/java/cz/cvut/fit/tjv/habitforgeserver/HabitforgeServerApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HabitforgeServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HabitforgeServerApplication.class, args);
 	}
-
 }
