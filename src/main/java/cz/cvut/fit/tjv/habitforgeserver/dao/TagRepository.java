ca611f9144568be8addf9e56d9d4fcc17a2fb8b9
Adam Berkeš <berkeada@fit.cvut.cz>
2024-01-02 18:30:18 +0100

    Add TagRepository dao

---
+++ src/main/java/cz/cvut/fit/tjv/habitforgeserver/dao/TagRepository.java
@@ -0,0 +1,9 @@
+package cz.cvut.fit.tjv.habitforgeserver.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import cz.cvut.fit.tjv.habitforgeserver.model.Tag;
+
+public interface TagRepository extends CrudRepository<Tag, Long> {
+
+}
