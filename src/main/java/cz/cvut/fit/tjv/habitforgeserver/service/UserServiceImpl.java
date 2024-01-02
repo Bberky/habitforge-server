c6ed3d4833b38bf183ec228681e211c418fc5dc8
Adam Berkeš <berkeada@fit.cvut.cz>
2024-01-02 18:35:06 +0100

    Remove unused import from UserServiceImpl

--- src/main/java/cz/cvut/fit/tjv/habitforgeserver/service/UserServiceImpl.java
+++ src/main/java/cz/cvut/fit/tjv/habitforgeserver/service/UserServiceImpl.java
@@ -1,7 +1,6 @@
 package cz.cvut.fit.tjv.habitforgeserver.service;
 
 import java.util.Collection;
-import java.util.Set;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.security.core.userdetails.UserDetails;
