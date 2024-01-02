73eaf37df564031bb3c5be741372d26a878b98c5
Adam Berkeš <berkeada@fit.cvut.cz>
2024-01-02 01:29:22 +0100

    Edit User entity, restrict access to id field to READ_ONLY and add validation annotations

--- src/main/java/cz/cvut/fit/tjv/habitforgeserver/model/User.java
+++ src/main/java/cz/cvut/fit/tjv/habitforgeserver/model/User.java
@@ -1,21 +1,23 @@
 package cz.cvut.fit.tjv.habitforgeserver.model;
 
 import java.util.Collection;
-import java.util.List;
 import java.util.Set;
 
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -28,54 +30,61 @@ import lombok.NoArgsConstructor;
 public class User implements DomainEntity<Long>, UserDetails {
     @Id
     @GeneratedValue
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Long id;
 
     private String fullName;
 
     @Column(nullable = false, unique = true)
+    @Email(message = "Email must be a valid email address.")
     private String email;
 
     @Column(nullable = false)
+    @NotBlank(message = "Password cannot be blank.")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
-    @ManyToMany(fetch = FetchType.EAGER)
-    private Set<Role> roles;
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private Collection<Habit> habits;
 
-    @OneToMany(mappedBy = "author")
-    private List<Habit> habits;
-
-    @Override
-    public Long getId() {
-        return id;
-    }
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private Collection<UserHabit> userHabits;
 
     @Override
+    @JsonIgnore
     public String getUsername() {
         return email;
     }
 
     @Override
+    @JsonIgnore
     public boolean isAccountNonExpired() {
         return true;
     }
 
     @Override
+    @JsonIgnore
     public boolean isAccountNonLocked() {
         return true;
     }
 
     @Override
+    @JsonIgnore
     public boolean isCredentialsNonExpired() {
         return true;
     }
 
     @Override
+    @JsonIgnore
     public boolean isEnabled() {
         return true;
     }
 
     @Override
+    @JsonIgnore
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return roles.stream().map(a -> new SimpleGrantedAuthority(a.getName())).toList();
+        return Set.of();
     }
 }
